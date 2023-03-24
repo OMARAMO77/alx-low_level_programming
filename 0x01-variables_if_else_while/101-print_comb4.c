d510dc40531259b458dcf4313c4cfadde8a2d1c1
Omar <omar0642624692@gmail.com>
2023-03-24 10:21:46 -0700

    different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,21 @@
 
 int main(void)
 {
-	int d, p, q;
+	int c, d, e;
 
-	for (d = '0'; d < '9'; d++)
+	for (c = '0'; c < '9'; c++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (d = '0'; d <= '9'; d++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (e = '0'; e <= '9'; e++)
 			{
-				if ((p != d) != q)
+				if (c < d && d < e)
 				{
+					putchar(c);
 					putchar(d);
-					putchar(p);
-					putchar(q);
+					putchar(e);
 
-					if (d == '7' && p == '8')
+					if (c == '7')
 						continue;
 
 					putchar(',');
