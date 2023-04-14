b65f4d68cc772480d6c0aa2e2359ebcc2c13ac24
Omar <omar0642624692@gmail.com>
2023-04-13 20:13:08 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,53 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+int checkdigit(char *p[], int ac);
 /**
- * main - Check the code
- * @argc: number of arguments
- * @argv: arguments
- * Return: Nothing
+ * main - multiply
+ * @argc: argument count
+ * @argv: argument array
+ * Return: result
  */
-int main(int argc, char **argv)
-{
-	int num1, num2, result;
 
-	if (argc != 3)
+int main(int argc, char *argv[])
+{
+	int mul, num1, num2;
+
+	if ((argc < 3 && argc > 4))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	if (checkdigit(argv, argc))
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
+/**
+ * checkdigit - check for digit
+ * @p: array pointer
+ * @ac: number of argument
+ * Return: failur or success
+ */
+
+int checkdigit(char *p[], int ac)
+{
+	int i, j;
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; p[i][j] != '\0'; j++)
+		{
+			if (!(p[i][j] >= '0' && p[i][j] <= '9'))
+				return (1);
+		}
+	}
+	return (0);
 }
