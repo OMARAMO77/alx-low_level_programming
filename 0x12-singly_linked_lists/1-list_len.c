627674f462376506581fbe5e4369bd0840a463f6
Omar <omar0642624692@gmail.com>
2023-04-27 17:07:30 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * list_len - returns the number of elements in a linked list_t list
+  * @h: linked list
+  *
+  * Return: number of elements
+  */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
