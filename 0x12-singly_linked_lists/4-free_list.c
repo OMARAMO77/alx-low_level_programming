cf709284661a7bf842d4f2c6a819ec2a3faa18e5
Omar <omar0642624692@gmail.com>
2023-04-27 18:35:01 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_list - Frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+  */
+void free_list(list_t *head)
+{
+	list_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head;
+		head = head->next;
+		free(temp_node);
+	}
+	free(head);
+}
