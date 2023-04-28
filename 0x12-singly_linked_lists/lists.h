88c86ffd7ed91ec88f4eb606ca3be7f8a69a0306
Omar <omar0642624692@gmail.com>
2023-04-27 17:00:57 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LIST_H
-#define _LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -23,4 +28,3 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
-
