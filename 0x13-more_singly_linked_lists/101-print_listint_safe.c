e6342d751a5818b8238c894aa5b2a4e52ee4813e
Omar <omar0642624692@gmail.com>
2023-05-03 16:49:38 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,71 +1,34 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list
+ * print_listint_safe - prints linked  list
  * @head: head of the linked list
- *
- * Return: number of nodes in the list.
- * If the function fails exit the program with status 98
+ * Return: number of nodes in the list
+ * If the function fails, exit the program with status 98
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_counter = 0;
-	newlist_t *temp_node, *new_node, *sum;
+	size_t nodes = 0;
+	const listint_t *first = head, *seconde = head;
 
-	temp_node = NULL;
-	while (head != NULL)
+	if (!head)
+		exit(98);
+
+	while (first && seconde && seconde->next && head)
 	{
-		new_node = malloc(sizeof(newlist_t));
-
-		if (new_node == NULL)
-			exit(98);
-
-		new_node->p = (void *)head;
-		new_node->next = temp_node;
-		temp_node = new_node;
-
-		sum = temp_node;
-
-		while (sum->next != NULL)
+		first = first->next;
+		seconde = seconde->next->next;
+		if (first == seconde)
 		{
-			sum = sum->next;
-			if (head == sum->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_newlist(&temp_node);
-				return (node_counter);
-			}
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
 		}
 
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		node_counter++;
+		nodes++;
 	}
-
-	free_newlist(&temp_node);
-	return (node_counter);
-}
-
-/**
- * free_newlist - function that frees a linked list
- * @head: head of a list.
- * Return: no return.
- */
-void free_newlist(newlist_t **head)
-{
-	newlist_t *temp_node, *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp_node = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp_node);
-		}
-		*head = NULL;
-	}
+	head = NULL;
+	return (nodes);
 }
