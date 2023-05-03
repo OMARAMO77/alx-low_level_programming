e86db5527aefe64aa93004e841ce913e3e732271
Omar <omar0642624692@gmail.com>
2023-05-02 17:47:40 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
  *
  * Return: nth node of a linked list, or NULL if the node does not exist
  */
- 
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
