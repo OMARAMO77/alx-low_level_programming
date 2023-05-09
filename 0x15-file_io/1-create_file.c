c7d3fe44de428a08c6c4ef9ec005d037a30f053a
Omar <omar0642624692@gmail.com>
2023-05-08 19:18:25 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int wr, fd;
 	int lenght = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-		write(fd, text_content, lenght);
-
+	{
+		wr = write(fd, text_content, lenght);
+		if (wr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
@@ -33,11 +39,12 @@ int create_file(const char *filename, char *text_content)
   *
   * Return: length of the string
   */
+
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (s)
+	while (*s)
 		len++;
 	return (len);
 }
