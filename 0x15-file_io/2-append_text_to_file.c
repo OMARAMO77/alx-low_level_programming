482e9405b9e79bf6cbbfae3b4e9530a7dc146945
Omar <omar0642624692@gmail.com>
2023-05-11 11:22:17 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,38 +10,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int lenght = _strlen(text_content);
+	int fd, rw;
+	int lenght = 0;
 
 	if (!filename)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		if (write(fd, text_content, lenght) == -1)
+		while (text_content[lenght])
+			lenght++;
+		rw = write(fd, text_content, lenght);
+		if (rw == -1)
 			return (-1);
 	}
-
 	close(fd);
 	return (1);
-}
-
-/**
-  * _strlen - returns the length of a string
-  * @s: string
-  *
-  * Return: length of the string
-  */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s)
-		len++;
-	return (len);
 }
