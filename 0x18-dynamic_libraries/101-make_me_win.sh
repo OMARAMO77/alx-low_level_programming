ee8b381bc5a8be276d55b20969586bd0199e1b2f
Omar <omar0642624692@gmail.com>
2023-06-24 12:15:39 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/OMARAMO77/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P .. https://github.com/OMARAMO77/alx-low_level_programming/raw/master/0x18-dynamic_libraries/omaramo.so
+export LD_PRELOAD="$PWD/../omaramo.so"
