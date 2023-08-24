75574cd40a564fe3466bbc520a0c8af707f00af0
Omar <omar0642624692@gmail.com>
2023-08-24 04:04:21 -0700

    a function that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,7 +18,7 @@ def island_perimeter(grid):
 
                 if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
-                
+
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
 
